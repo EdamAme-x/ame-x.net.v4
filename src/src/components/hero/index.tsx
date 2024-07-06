6319255bbf5dfaec6ceb45dad3579a8530758ceb
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-07-06 14:53:11 +0900

    add

--- src/src/components/hero/index.tsx
+++ src/src/components/hero/index.tsx
@@ -1,16 +1,17 @@
+import Image from "next/image";
 import clsx from "clsx";
+import GithubVector from "@/assets/svg/github.svg";
+import XVector from "@/assets/svg/x.svg";
 import { GlassedButton } from "../ui/glassed-button";
 import { HoverPreview } from "../ui/hover-preview";
 import { Sparkles } from "../ui/sparkles";
 import { Spotlight } from "../ui/spotlight";
-import GithubVector from "@/assets/svg/github.svg";
-import XVector from "@/assets/svg/x.svg";
-import Image from "next/image";
+import { Rocket } from "./rocket";
 
 export function Hero() {
 	return (
-		<div className={clsx("mt-14")}>
-			<Spotlight fill="#0FAD78" className="top-0" />
+		<div className={clsx("pt-14")}>
+			{/* <Spotlight fill="#0FAD78" className="top-0" />
 			<Sparkles
 				id="tsparticlesfullpage"
 				background="transparent"
@@ -19,48 +20,70 @@ export function Hero() {
 				particleDensity={100}
 				className="w-full h-screen"
 				particleColor="#0FAD78"
-			/>
+			/> */}
 			<div
 				className={clsx(
 					"absolute",
 					"top-0 left-1/2 right-0 bottom-0",
 					"transform -translate-x-1/2",
 					"z-10",
-					"flex flex-col justify-center items-start"
+					"flex justify-center items-center",
+					"w-screen h-screen"
 				)}>
-				<h1
-					className={clsx(
-						"text-3xl font-bold font-raleway",
-						"inline-flex flex-col"
-					)}>
-					<span
-						className={clsx(
-							"text-3xl",
-							"bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50"
-						)}>
-						Welcome to
-					</span>{" "}
-					<span
-						className={clsx(
-							"text-6xl text-akin-bold",
-							"text-gradient",
-							"animation-hue-rotate"
-						)}>
-						AMEX NET.
-					</span>
-				</h1>
-				<div className="mt-3 flex gap-4 justify-between items-center w-1/2">
-					<GlassedButton>
-						<span>About me</span>
-					</GlassedButton>
-					<div className="flex gap-4 items-center mr-5">
-						<HoverPreview className="text-white" url="https://github.com/EdamAme-x">
-                            <Image src={GithubVector} alt="github" width={24} height={24} />
-						</HoverPreview>
-                        <HoverPreview className="text-white" url="https://x.com/amex2189">
-                            <Image src={XVector} alt="x" width={24} height={24} />
-						</HoverPreview>
+				<div className="w-1/2 flex flex-col justify-center items-center">
+					<div className="flex flex-col">
+						<h1
+							className={clsx(
+								"text-3xl font-bold font-raleway",
+								"inline-flex flex-col"
+							)}>
+							<span
+								className={clsx(
+									"text-3xl",
+									"bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50"
+								)}>
+								Welcome to
+							</span>{" "}
+							<span
+								className={clsx(
+									"text-6xl text-akin-bold",
+									"text-gradient",
+									"animation-hue-rotate"
+								)}>
+								AMEX NET.
+							</span>
+						</h1>
+						<div className="mt-3 flex items-center gap-4 justify-between">
+							<GlassedButton>
+								<span>About me</span>
+							</GlassedButton>
+							<div className="flex gap-4 items-center pr-5">
+								<HoverPreview
+									className="text-white"
+									url="https://github.com/EdamAme-x">
+									<Image
+										src={GithubVector}
+										alt="github"
+										width={24}
+										height={24}
+									/>
+								</HoverPreview>
+								<HoverPreview
+									className="text-white"
+									url="https://x.com/amex2189">
+									<Image
+										src={XVector}
+										alt="x"
+										width={24}
+										height={24}
+									/>
+								</HoverPreview>
+							</div>
+						</div>
 					</div>
+				</div>
+				<div className="w-1/2 h-screen">
+					<Rocket />
 				</div>
 			</div>
 		</div>
---
+++ src/src/components/hero/rocket.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+export function Rocket() {
+	const canvasRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (!canvasRef.current) return;
+		if (canvasRef.current.children.length > 0) return;
+
+		const scene = new THREE.Scene();
+		const camera = new THREE.PerspectiveCamera(
+			75,
+			window.innerWidth / window.innerHeight,
+			0.1,
+			1000
+		);
+		const renderer = new THREE.WebGLRenderer({
+			antialias: true,
+			alpha: true
+		});
+		renderer.setClearColor(0x000000, 0);
+		renderer.setSize(window.innerWidth / 2, window.innerHeight);
+		canvasRef.current.appendChild(renderer.domElement);
+
+		const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
+		scene.add(ambientLight);
+
+		const directionalLight = new THREE.DirectionalLight(0xffffff, 10);
+		directionalLight.position.set(0, 5, 5);
+		scene.add(directionalLight);
+
+		let model: THREE.Group<THREE.Object3DEventMap> | null;
+
+		const loader = new GLTFLoader();
+		loader.load("/data/rocket/scene.gltf", function (gltf) {
+			model = gltf.scene;
+			const scale = 1.75;
+			model.scale.set(4 * scale, 4 * scale, 4 * scale);
+			model.rotateY(-1.5);
+			model.rotateZ(0.5);
+			model.rotateX(-0.75);
+			scene.add(model);
+		});
+
+		// const controls = new OrbitControls(camera, renderer.domElement);
+		// controls.enableDamping = true;
+		// controls.dampingFactor = 0.25;
+		// controls.enableZoom = true;
+
+		camera.position.set(0, 0, 10);
+
+		let frame = 0;
+
+		function animate() {
+			frame += 10;
+			requestAnimationFrame(animate);
+			if (frame % 15 === 0) {
+				// controls.update();
+				if (model) {
+                    model.rotation.x += 0.05;
+                    model.rotation.z += 0.05;
+				}
+				renderer.render(scene, camera);
+			} else {
+				if (frame > 6000) {
+					frame = 0;
+				}
+			}
+		}
+		animate();
+
+		window.addEventListener("resize", function () {
+			camera.aspect = window.innerWidth / window.innerHeight;
+			camera.updateProjectionMatrix();
+			renderer.setSize(window.innerWidth / 2, window.innerHeight);
+		});
+
+		return () => {
+			canvasRef.current?.removeChild(renderer.domElement);
+		};
+	});
+
+	return <div ref={canvasRef}></div>;
+}
